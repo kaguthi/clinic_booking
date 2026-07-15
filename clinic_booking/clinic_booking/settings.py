72a740dcb494373d03400f6d1fd49fa9921e28be
kaguthi <peterkaguathi873@gmail.com>
2026-07-15 13:57:31 +0300

    Update CI/CD workflow and settings for environment configuration
    - Set working directory in CI/CD workflow
    - Update DEBUG setting in Django settings to use environment variable

--- clinic_booking/clinic_booking/settings.py
+++ clinic_booking/clinic_booking/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -73,16 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'clinic_booking.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
   'default': {
